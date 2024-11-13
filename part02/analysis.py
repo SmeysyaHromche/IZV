0abe5852958a206f2aab496c06afae4516bb00c8
SmeysyaHromche <MyrKykhta@gmail.com>
2024-11-13 10:39:25 +0100

    parse_data

--- part02/analysis.py
+++ part02/analysis.py
@@ -13,14 +13,35 @@ import zipfile
 # Ukol 1: nacteni dat ze ZIP souboru
 
 
-def load_data(filename: str) -> pd.DataFrame:
-    pass
+def load_data(filename : str, ds : str) -> pd.DataFrame:
+    dfs = []
+    with zipfile.ZipFile(filename, 'r') as trg_zip:
+        for item in trg_zip.namelist():
+            if ds in item:
+                with trg_zip.open(item) as trg_file:
+                    dfs.extend(pd.read_html(trg_file, encoding='cp1250'))
+    return pd.concat(dfs)
 
 # Ukol 2: zpracovani dat
 
 
 def parse_data(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
-    pass
+    new_df = df.copy()
+    
+    new_df['date'] = pd.to_datetime(df['p2a'], dayfirst=True, yearfirst=False, format=None, errors='coerce')
+    
+    region_dict = {0: "PHA", 1: "STC", 2: "JHC", 3: "PLK", 4: "ULK", 5: "HKK",6:
+    "JHM", 7: "MSK", 14: "OLK", 15: "ZLK", 16: "VYS", 17: "PAK", 18:
+    "LBK", 19: "KVK"}
+    new_df['region'] = new_df['p4a'].apply(lambda x: region_dict[x]) 
+
+    new_df.drop_duplicates(subset=['p1'], inplace=True)
+    
+    if verbose:
+        new_size = new_df.memory_usage(deep=True).sum()/1_000_000
+        print(f'new_size={new_size} MB')
+    
+    return new_df
 
 # Ukol 3: počty nehod v jednotlivých regionech podle stavu vozovky
 def plot_state(df: pd.DataFrame, fig_location: str = None,
