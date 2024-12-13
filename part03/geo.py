b81a645f49baa1eaa8deaf1b9487e9eb2ff2b45a
SmeysyaHromche <MyrKykhta@gmail.com>
2024-12-13 16:03:50 +0100

    3.0: update

--- part03/geo.py
+++ part03/geo.py
@@ -3,26 +3,36 @@
 # %%%
 import pandas as pd
 import geopandas
+import matplotlib
 import matplotlib.pyplot as plt
 import contextily
 import sklearn.cluster
 import numpy as np
 
+matplotlib.use('TkAgg')
+
 def make_geo(df_accidents: pd.DataFrame, df_locations: pd.DataFrame) -> geopandas.GeoDataFrame:
     
     #Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani Pozor na mozne prohozeni d a e!
+
     pass
 
 def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
              show_figure: bool = False):
     # Vykresleni grafu s nehodami s vlivem alogolu (4>) v kraji  """
-    pass
+    if fig_location:
+        plt.savefig(fig_location)
+    if show_figure:
+        plt.show()
 
 
 def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                  show_figure: bool = False):
     # Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru
-    pass
+    if fig_location:
+        plt.savefig(fig_location)
+    if show_figure:
+        plt.show()
 
 if __name__ == "__main__":
     # zde muzete delat libovolne modifikace
