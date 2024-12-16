d67b524339f0615fc9efb89cbb871ae64833743a
SmeysyaHromche <MyrKykhta@gmail.com>
2024-12-16 20:41:12 +0100

    3.0: 3

--- part03/geo.py
+++ part03/geo.py
@@ -6,8 +6,11 @@ import geopandas
 import matplotlib
 import matplotlib.pyplot as plt
 import contextily as ctx
-import sklearn.cluster
+from sklearn.cluster import DBSCAN, KMeans, AgglomerativeClustering
+from sklearn.preprocessing import StandardScaler
 import numpy as np
+from shapely.geometry import box
+from torch import unique
 
 matplotlib.use('TkAgg')
 
@@ -23,7 +26,7 @@ def make_geo(df_accidents: pd.DataFrame, df_locations: pd.DataFrame) -> geopanda
     """
     df = pd.merge(df_accidents, df_locations, on='p1')  # merge accidents and geo data to one frame
     df = df[(df['d'].notna()) & (df['e'].notna()) & (df['d'] != 0) & (df['e'] != 0)]  # clean d and e col
-    df['d'], df['e'] = np.where(df['d']> df['e'],
+    df['d'], df['e'] = np.where(df['d'] > df['e'],
                                 (df['d'].values, df['e'].values),
                                 (df['e'].values, df['d'].values))  # check swapped d and e data
     return geopandas.GeoDataFrame(df,
@@ -45,7 +48,6 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     # aux const
     region = 'JHM'
     month = {1:'Leden', 2:'Unor'}
-    color = 'red'
 
     # data preparing
     df = gdf[gdf['region'] == region]  # filter by trg region
@@ -57,11 +59,11 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(15, 12))
     plt.tight_layout()
     for i, k in enumerate(month.keys()):
-        df[df['date'].dt.month == k].plot(ax=ax[i], color=color, markersize=5, alpha=0.6)
+        df[df['date'].dt.month == k].plot(ax=ax[i], color='red', markersize=5, alpha=0.6)
         ax[i].set_xlim(xmin=minx, xmax=maxx)
         ax[i].set_ylim(ymin=miny, ymax=maxy)
         ax[i].set_title(f'{region} kraj - pod vlivem alkoholu ({month[k]})')
-        ctx.add_basemap(ax[i], crs=gdf.crs.to_string(), source=ctx.providers.OpenStreetMap.Mapnik, alpha=0.9, zoom=10)
+        ctx.add_basemap(ax[i], crs=df.crs.to_string(), source=ctx.providers.OpenStreetMap.Mapnik, alpha=0.9, zoom=10)
         ax[i].set_axis_off()
 
     # save and display
@@ -73,7 +75,59 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
 
 def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                  show_figure: bool = False):
-    # Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru
+    """
+    Create accident by animals charts in geo representation with Agglomerative clustering and save it on target file
+    with optional displaying.
+
+    Args:
+        gdf: accident and geo data
+        fig_location: path/name for target file for saving. Defaults to None.
+        show_figure: flag for optional displaying. Defaults to False.
+
+    Returns:
+        None
+    """
+    # aux
+    region = 'JHM'
+
+    # data preparing
+    df = gdf[gdf['region'] == region]  # filter by trg region
+    df = df[df['p10'] == 4]  # filter by animals
+    (minx, miny, maxx, maxy) = df.total_bounds
+
+    # Clustering
+    # ==========
+    # I didn't want to limit myself to K-means clustering, so I decided to try other approaches.
+    # The first one I tried was DBSCAN, but I had problems with data normalisation, esp selection and uneven data density.
+    # Based on this, I make choose on Agglomerative clustering, it showed good test data distributions in my opinion.
+    # Through experimentation, I found an average number of clusters that would divide the plane logically enough
+    # without splitting it into smaller parts. I choose for linkage method 'ward' for better data compatibility.
+    # ===========
+    df['cluster'] = AgglomerativeClustering(n_clusters=12, linkage='ward').fit_predict(df[['d', 'e']])
+    areas = df.groupby('cluster')['geometry'].apply(
+        lambda x: x.unary_union.convex_hull).reset_index()
+    polygons = geopandas.GeoDataFrame(geometry=areas['geometry'], crs=df.crs)
+    polygons['accident_count'] = polygons.apply(
+        lambda x: df[df.geometry.within(x.geometry)].shape[0], axis=1)
+
+    # plotting
+    fig, ax = plt.subplots(figsize=(15, 10))
+    plt.tight_layout()
+    ax.set_xlim(xmin=minx, xmax=maxx)
+    ax.set_ylim(ymin=miny, ymax=maxy)
+    polygons.plot(ax=ax, column='accident_count', cmap='plasma', alpha=0.3)
+    df.plot(ax=ax, color='red', markersize=5, alpha=0.6)
+
+    # create colorbar based on number of accident in every cluster region
+    norm = matplotlib.colors.Normalize(vmin=polygons['accident_count'].min(), vmax=polygons['accident_count'].max())
+    cmap = matplotlib.cm.ScalarMappable(norm=norm, cmap='plasma')
+    plt.colorbar(cmap, ax=ax, orientation='horizontal', pad=0.02, label='Počet nehod v úseku')
+
+    # map
+    ctx.add_basemap(ax, crs=df.crs.to_string(), source=ctx.providers.OpenStreetMap.Mapnik, alpha=0.9, zoom=10)
+    ax.set_axis_off()
+    ax.set_title(f'Nehody v {region} kraji zaviněné lesní zvěří')
+
     if fig_location:
         plt.savefig(fig_location)
     if show_figure:
@@ -85,9 +139,4 @@ if __name__ == "__main__":
     df_locations = pd.read_pickle("locations.pkl.gz")
     gdf = make_geo(df_accidents, df_locations)
     plot_geo(gdf, "geo1.png", True)
-    #plot_cluster(gdf, "geo2.png", True)
-    #
-    # # testovani splneni zadani
-    # import os
-    # assert os.path.exists("geo1.png")
-    # assert os.path.exists("geo2.png")
+    plot_cluster(gdf, "geo2.png", True)
